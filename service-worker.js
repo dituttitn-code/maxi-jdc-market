d3a657849a372eb32b763583a34b3f5b269da5a8
dituttitn-code <ditutti.tn@gmail.com>
2025-12-17 03:31:52 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,34 @@
+const CACHE_NAME = "maxi-jdc-cache-v1";
+
+const ASSETS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./products.json",
+  "./logo-maxi-jdc-market.jpg",
+  "./qr-maxi-jdc-site.jpg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => key !== CACHE_NAME && caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
 
