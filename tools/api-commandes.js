27c9f5384102d61c029f0fca0843d767606269ec
dituttitn-code <ditutti.tn@gmail.com>
2026-01-30 12:34:18 +0100

    Update api-commandes.js

--- tools/api-commandes.js
+++ tools/api-commandes.js
@@ -4,7 +4,7 @@
 
 // ✅ URL WebApp Apps Script (⚠️ sans espace au début)
 const API_URL =
-  "https://script.google.com/macros/s/AKfycbxmmAHNTR1zy7fAGbJmu2ci1WTp9MeZduHA3wVBHin9cTN9KyrLshjOvIdOdMHl_Zcw/exec";
+  "https://script.google.com/macros/s/AKfycbzDmdNfwX4xlmM70vqm3qP41mIkJva_JZPlKP27JXewJ2gybikWlcvUH8J3B8ewqeUu/exec";
 
 // ✅ Token (doit être EXACTEMENT le même que dans Code.gs)
 // (Si côté serveur TOKEN_OPTIONNEL=true, il ne bloque pas même si token faux/vide)
