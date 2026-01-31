53fa44758ecf2f4d516355454ae040bc7923d617
dituttitn-code <ditutti.tn@gmail.com>
2026-01-31 17:43:00 +0100

    Update config.js

--- tools/config.js
+++ tools/config.js
@@ -1,4 +1,3 @@
-// config.js
 const APP_CONFIG = {
   googleScriptUrl: "https://script.google.com/macros/s/AKfycby3acB0gMMk-Ub8CXt__33nH0NNIrYfPeU3DEiLmUCLDW-RMLnPc_F_5eDjLYnxDu8/exec",
   minOrder: 15,
