1d27c8ab717e1058d0adee432e8a68cd9fda38ee
dituttitn-code <ditutti.tn@gmail.com>
2026-01-31 16:06:52 +0100

    Update config.js

--- tools/config.js
+++ tools/config.js
@@ -1,20 +1,10 @@
-// config.js - VERSION CORRIGÉE
 const APP_CONFIG = {
-  // URL de votre Google Apps Script
-  googleScriptUrl: "https://script.google.com/macros/s/AKfycbzavyRmmqMZBTansAtWwCr8-XW4Pa1PWrFyM0pg1TlVvKPK1hoPaMchyadWkOCda9eq/exec",
-  
-  // Configuration existante
+  googleScriptUrl: "https://script.google.com/macros/s/AKfycby3acB0gMMk-Ub8CXt__33nH0NNIrYfPeU3DEiLmUCLDW-RMLnPc_F_5eDjLYnxDu8/exec",
   minOrder: 15,
   freeShipping: 100,
   shippingFee: 3,
   whatsappNumber: "21625600978",
   itemsPerPage: 24,
-  
-  // Méthode pour obtenir l'URL
-  getScriptUrl: function() {
-    return this.googleScriptUrl;
-  }
+  getScriptUrl: function() { return this.googleScriptUrl; }
 };
-
-// Rendre la configuration accessible globalement
 window.APP_CONFIG = APP_CONFIG;
