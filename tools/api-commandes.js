83842c9221a215592289ae736c2515864713d131
dituttitn-code <ditutti.tn@gmail.com>
2026-02-02 21:08:11 +0100

    Update api-commandes.js

--- tools/api-commandes.js
+++ tools/api-commandes.js
@@ -4,7 +4,7 @@
 
 // ⚠️ Mets ICI la même URL que ta page suivi (celle qui marche chez toi)
 const API_URL =
-  "https://script.google.com/macros/s/AKfycby7j8LJrO0uOSh5z5NEIvaQSg_vjhSqhofEimhZe9U8RPqLkA4GYxXYeFuOKiq7JLWV/exec";
+  "https://script.google.com/macros/s/AKfycbzT8NrDgGALXNu04OFhQLO-aMKk-PdDsEB0B3ALO69sWzD-qLpxFL79vY_rJQtBSkyw/exec";
 
 /*********************************
  * ENVOYER UNE COMMANDE (ECRITURE)
