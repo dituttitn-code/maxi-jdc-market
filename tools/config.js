bab30b8889633224bf8236341d3cf67dedb865b3
dituttitn-code <ditutti.tn@gmail.com>
2026-02-02 19:58:39 +0100

    Update config.js

--- tools/config.js
+++ tools/config.js
@@ -5,7 +5,7 @@
 window.APP_CONFIG = window.APP_CONFIG || {};
 
 // URL du Google Apps Script (METTEZ VOTRE URL ICI)
-window.APP_CONFIG.googleScriptUrl = "https://script.google.com/macros/s/AKfycbzpIBKhYI5oKrCmdSAy1f5kdLkJ0n-f5CxdwL5EBKnvZ7kILUhBdQ7-BByWhfEgqHQN/exec";
+window.APP_CONFIG.googleScriptUrl = "https://script.google.com/macros/s/AKfycbyuF6yfYVFkIXG-B3qAFWWn3K67iZVdlx_QmMcNQALi_nepennRdDvt1j_Rb_24Odqb/exec";
 
 // Fonction pour obtenir l'URL du script
 window.APP_CONFIG.getScriptUrl = function() {
