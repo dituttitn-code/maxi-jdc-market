371e99c5042f6c76752d206f6c3cf093d3cb8dd7
dituttitn-code <ditutti.tn@gmail.com>
2026-02-12 14:54:23 +0100

    Update config.js

--- tools/config.js
+++ tools/config.js
@@ -7,7 +7,7 @@
 /**
  * 1) Mets ici l'URL de ton WebApp Apps Script (le même que tu utilises déjà)
  * Exemple:
- * https://script.google.com/macros/s/AKfycbwcpRAbF9x8dHv_jla082is6_heQOCcDui-ozY5AhQ83Zj8QRAUKcYqh1cN3Pi8Xwv8/exec
+ * https://script.google.com/macros/s/AKfycbz7SnlINFan7J706C5K4RDaFNqCjh-JywfrLVXhVFrdj-UteFVOPAAUBTV880IQCgyj/exec
  */
 const APPS_SCRIPT_URL = "PUT_YOUR_APPS_SCRIPT_WEBAPP_URL_HERE";
 
