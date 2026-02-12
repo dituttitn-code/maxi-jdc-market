1e622969da72fc91b79725a48e6da52bd30a8ea7
dituttitn-code <ditutti.tn@gmail.com>
2026-02-12 19:26:58 +0100

    Update api-commandes.js

--- tools/api-commandes.js
+++ tools/api-commandes.js
@@ -6,7 +6,7 @@
 
 // ✅ URL OK
 export const API_URL =
-  "https://script.google.com/macros/s/AKfycbyPlPa-gUorOjsB01nWDiAgti7BW70xtqLN49oTMeeq8nvErGAxfOeqkzDCsKRfSvbM/exec";
+  "https://script.google.com/macros/s/AKfycbzAaRtg24yHPqLQs5_q6XNdlKsgLWCe-lQPdHCalmUlrnrrVC9o1HDfnqdlQ_IpgzpV/exec";
 
 /*********************************
  * ENVOYER UNE COMMANDE (ECRITURE)
