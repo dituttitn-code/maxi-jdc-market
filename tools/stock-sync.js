a3c56a3319293af36acba6996c9a4f2d6aa3af04
dituttitn-code <ditutti.tn@gmail.com>
2026-03-11 01:28:49 +0100

    Update stock-sync.js

--- tools/stock-sync.js
+++ tools/stock-sync.js
@@ -1,1 +1,8 @@
+const STOCK_API_URL = "https://script.google.com/macros/s/AKfycbx1T7J7RRslouJ3l03rXY7VPVloW-MUrvV_mbj5GlRWRxSv8XnR2osWYydwnfKo05YISA/exec";
 
+async function testStockSync() {
+  const response = await fetch(STOCK_API_URL);
+  const data = await response.json();
+  console.log("Stock API OK:", data);
+  return data;
+}
