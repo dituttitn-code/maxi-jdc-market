43bfbe108c867d13dce9d896861fa37a2f76443c
dituttitn-code <ditutti.tn@gmail.com>
2026-03-25 03:18:26 +0100

    Update stock-sync.js

--- tools/stock-sync.js
+++ tools/stock-sync.js
@@ -1,5 +1,5 @@
 const STOCK_API_URL =
-  "https://script.google.com/macros/s/AKfycbziH-sLBmTksNcSookG2XZtFo5ltPx8pojVO02nbPrjt-KB4uJQYwLoAOKDZ-yZiH9_IA/exec?action=getProducts";
+  "https://script.google.com/macros/s/AKfycbzvrDMk1KXkKWjxIsutdPdFvodrW_OY5MNQEjrll8AyiOUWa6Mk8v4owmKUUPRwUJnKcQ/exec";
 
 const SYNC_INTERVAL_MS = 30000;
 const DEFAULT_CATEGORY = "Épicerie Salée";
