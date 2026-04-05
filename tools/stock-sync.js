85d1ef18b8c4f4d3f1397384d60cff60c22872d0
dituttitn-code <ditutti.tn@gmail.com>
2026-04-05 23:12:00 +0100

    Update stock-sync.js

--- tools/stock-sync.js
+++ tools/stock-sync.js
@@ -1,5 +1,5 @@
 // REMPLACEZ CETTE URL par votre URL de déploiement la plus récente
-const STOCK_API_URL = "https://script.google.com/macros/s/AKfycbzAT1hKBZlpwJSnP83rWxuq1pu7OXDgEwLcBAIp3lvz9iaKwpzEK3a-gvftlWnJ2W36vQ/exec";
+const STOCK_API_URL = "https://script.google.com/macros/s/AKfycbxHMMCWH44NIk0NJT1eBONciTJ-xLVUf3mCkbVdPHW3hn-PWpl8-sWYZPOfaoi_YDrr/exec";
 
 const SYNC_INTERVAL_MS = 30000;
 
